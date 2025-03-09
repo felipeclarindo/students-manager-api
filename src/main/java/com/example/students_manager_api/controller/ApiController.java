67064ae559ddce64047a393c303608594374b0c2
felipeclarindo <fglpc2@gmail.com>
2025-03-09 15:02:25 -0300

    feat(api): Creatting ApiController to give api details.

---
+++ src/main/java/com/example/students_manager_api/controller/ApiController.java
@@ -0,0 +1,26 @@
+package com.example.students_manager_api.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+    @GetMapping("/api")
+    public ResponseEntity<Object> index() {
+        Map<String, String> response = new HashMap<>();
+        
+        response.put("message", "Welcome to the Students Manager API");
+        response.put("status", "API is running");
+        response.put("description", "This API is a simple CRUD to manage students");
+        response.put("author", "Felipe Clarindo");
+        response.put("author-github", "https://github.com/felipeclarindo");
+        response.put("version", "1.0.0");
+        response.put("repository", "https://github.com/felipeclarindo/students-manager-api");
+
+        return ResponseEntity.status(200).body(response);
+    }
+}

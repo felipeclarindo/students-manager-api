7f2cd0d225c8f6ba50f819264be47c5a230c29ed
Felipe Clarindo <75388462+felipeclarindo@users.noreply.github.com>
2025-03-09 15:38:49 -0300

    feat(application): add the First Controller with the methods getAll, getById and create (Fixes #1)
    Logs Development
    
    * feat(model): Adding student class.
    
    * feat(controller): Start development of the application controller.
    
    * feat(api): Creatting ApiController to give api details.
    
    * refactor(model): Updating the atribbutes of the student class.
    
    * feat(controller): Implements the api funcionality in cache.

---
+++ src/main/java/com/example/students_manager_api/model/Student.java
@@ -0,0 +1,27 @@
+package com.example.students_manager_api.model;
+
+import java.util.List;
+
+public class Student {
+    private Long id;
+    private String name;
+    private List<Number> notes;
+
+    public Student(Long id, String name, List<Number> notes) {
+        this.id = id;
+        this.name = name;
+        this.notes = notes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public List<Number> getNotes() {
+        return notes;
+    }
+}

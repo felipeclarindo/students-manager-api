d1e590caa274749df9889621f418fe58eb91a6c4
Felipe Clarindo <75388462+felipeclarindo@users.noreply.github.com>
2025-03-24 23:26:13 -0300

    feat(api): Adding the methods (delete, put and getById. (Fixes  #4)

--- src/main/java/com/example/students_manager_api/controller/StudentController.java
+++ src/main/java/com/example/students_manager_api/controller/StudentController.java
@@ -7,13 +7,18 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.students_manager_api.model.Student;
 
@@ -29,11 +34,11 @@ public class StudentController {
             Map<String, Object> response = new HashMap<>();
     
             response.put("content", repository);
-            response.put("message", "Students successfully found");
-            return ResponseEntity.status(200).body(response);
+            response.put("message", "Students founded with successfully.");
+            return ResponseEntity.ok(response);
         }
         Map<String, Object> response = new HashMap<>();
-        response.put("message", "No students found");
+        response.put("message", "No students found.");
         response.put("content", Collections.emptyList());
         return ResponseEntity.status(200).body(response);
     }
@@ -44,14 +49,15 @@ public class StudentController {
 
         for (Student s : repository) {
             if (s.getId().equals(student.getId())) {
-                response.put("message", "Student already exists");
+                response.put("message", "Student already exists.");
                 return ResponseEntity.status(409).body(response);
             }
         }
 
         repository.add(student);
-        response.put("message", "Student successfully created");
+        response.put("message", "Student created with successfully.");
         response.put("content", student);
+
         return ResponseEntity.status(201).body(response);
     }
 
@@ -65,12 +71,49 @@ public class StudentController {
 
         if (student.isPresent()) {
             Map<String, Object> response = new HashMap<>();
-            response.put("message", "Student successfully found");
+            response.put("message", "Student founded with successfully.");
             response.put("content", student);
             return ResponseEntity.status(200).body(response); 
         }
         Map<String, Object> response = new HashMap<>();
-        response.put("message", "Student not found");
+        response.put("message", "Student not found.");
         return ResponseEntity.status(404).body(response);
+    }
+
+    
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Object> destroy(@PathVariable Long id) {
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("message", "Student deleted with suscessfully.");
+        
+        repository.remove(getStudent(id));
+        
+        return ResponseEntity.status(200).body(response);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Object> update(@PathVariable Long id, @RequestBody Student student) {
+        Student existingStudent = getStudent(id);
+        repository.remove(existingStudent);
+
+        student.setId(id);
+        repository.add(student);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Student updated with successfully.");
+        response.put("content", student);
+
+        return ResponseEntity.status(200).body(response);
+    }
+
+    private Student getStudent(Long id) {
+        return repository.stream()
+                .filter(c -> c.getId().equals(id))
+                .findFirst()
+                .orElseThrow(
+                    () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Student with id " + id + " not founded.")
+                );
     }
 }

6ddbbe663a5d6dd829c546d6e476214d69909f6f
felipeclarindo <fglpc2@gmail.com>
2025-04-13 02:45:09 -0300

    feat(model): Adding model to make the response of the api description endpoint.

--- src/main/java/com/example/students_manager_api/model/Student.java
+++ src/main/java/com/example/students_manager_api/model/Student.java
@@ -2,42 +2,39 @@ package com.example.students_manager_api.model;
 
 import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Name is needed.")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
     private String name;
+
+    @NotBlank(message = "Course is needed.")
+    @Size(min = 3, max = 50, message = "Course must be between 3 and 50 characters.")
     private String course;
+
+    @NotBlank(message = "Period is needed.")
+    @Size(min = 3, max = 50, message = "Period must be between 3 and 50 characters.")
     private String period;
+
+    @NotBlank(message = "Notes are needed.")
     private List<Number> notes;
-
-    public Student(Long id, String name, String course, String period, List<Number> notes) {
-        this.id = id;
-        this.name = name;
-        this.course = course;
-        this.period = period;
-        this.notes = notes;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getCourse() {
-        return course;
-    }
-
-    public List<Number> getNotes() {
-        return notes;
-    }
-
-    public String getPeriod() {
-        return period;
-    }
 }

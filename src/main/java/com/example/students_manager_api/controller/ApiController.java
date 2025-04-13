f8ab23b38ba203c0f97bb6f151b0159029ae025d
felipeclarindo <fglpc2@gmail.com>
2025-04-13 02:47:09 -0300

    refactor(controller): Refactor the controller to fix errors and bugs.

--- src/main/java/com/example/students_manager_api/controller/ApiController.java
+++ src/main/java/com/example/students_manager_api/controller/ApiController.java
@@ -1,29 +1,25 @@
 package com.example.students_manager_api.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.students_manager_api.model.ApiResponseDescription;
+
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-    
+
     @GetMapping
     public ResponseEntity<Object> index() {
-        Map<String, String> response = new HashMap<>();
-        
-        response.put("message", "Welcome to the Students Manager API");
-        response.put("status", "API is running");
-        response.put("description", "This API is a simple CRUD to manage students");
-        response.put("author", "Felipe Clarindo");
-        response.put("author-github", "https://github.com/felipeclarindo");
-        response.put("version", "1.0.0");
-        response.put("repository", "https://github.com/felipeclarindo/students-manager-api");
+        ApiResponseDescription apiDescription = new ApiResponseDescription();
 
-        return ResponseEntity.status(200).body(response);
+        apiDescription.setMessage("Welcome to the Students Manager API").setStatus("API is running")
+                .setDescription("This API is a simple CRUD to manage students.").setAuthor("Felipe Clarindo")
+                .setGithubAuthor("https://github.com/felipeclarindo").setVersion("1.0.0")
+                .setRepositoryUrl("https://github.com/felipeclarindo/students-manager-api");
+
+        return ResponseEntity.status(200).body(apiDescription);
     }
 }
